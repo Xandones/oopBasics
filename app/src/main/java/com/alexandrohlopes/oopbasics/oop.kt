bb85e3e061b404da1c74bcc89e348ce20ed8df9d
Alexandro H. Lopes <karkekoisaaeh@outlook.com>
2022-09-29 18:20:53 -0300

    Added Lateinit plus Getters and Setters

--- app/src/main/java/com/alexandrohlopes/oopbasics/oop.kt
+++ app/src/main/java/com/alexandrohlopes/oopbasics/oop.kt
@@ -4,6 +4,13 @@ fun main() {
     var alexandro = Person("Alexandro", "Hugo Lopes", 40) // Creates a new instance overwriting the data in the main constructor
     alexandro.stateHobby() // The instance will receive as hobby attribute whatever is given on the constructor.
     println("Alexandro's age is ${alexandro.age}")
+    var alexandroCar = Car()
+    alexandroCar.owner = "Alexandro"
+    alexandroCar.brand = "Lamborghini"
+    alexandroCar.maxSpeed = 400
+    //alexandroCar.model = "Veneno" // You cannot alter the attribute because it's private - it can only be altered inside the class.
+    println("${alexandroCar.owner}'s car is a ${alexandroCar.brand} with a max speed of ${alexandroCar.maxSpeed} and it's a ${alexandroCar.model}")
+    // Since the getter isn't private, you can read the data of the attribute.
     println()
 
     // The following instances (johnDoe and johnPeterson) aren't using the secondary constructor.
@@ -22,7 +29,7 @@ class Person (firstName : String = "John", lastName : String = "Doe") {
 
     // Attributes
     var age : Int? = null
-    var hobby : String = "Watch Crunchyroll"
+    var hobby : String = "to watch Crunchyroll"
     var firstName : String? = null
 
     init { // Class initializer. This code will run everytime an instance is created.
@@ -38,6 +45,29 @@ class Person (firstName : String = "John", lastName : String = "Doe") {
 
     // Methods
     fun stateHobby() {
-        println("$firstName\'s hobby is $hobby")
+        println("$firstName's hobby is $hobby")
+    }
+}
+
+class Car() {
+    lateinit var owner : String // Allows setting the variable later. If the developer forgets it, the compiler will throw an exception.
+
+    var brand : String = "BMW"
+    get() { // Custom getter
+        return field.lowercase() // Converts the brand name to lowercase letters.
+    }
+
+    var maxSpeed : Int = 200
+    // Kotlin sets default getters and setters that cover most uses.
+    set(value) {
+        field = if (value > 0) value else throw java.lang.IllegalArgumentException("Max speed cannot be lower than zero.")
+    }
+
+    var model : String = "Beetle"
+        private set
+
+    init {
+        this.owner = "Frank"
+        this.model = "Veneno"
     }
 }
